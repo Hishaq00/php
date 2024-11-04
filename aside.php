fc59b9f97d0335c576a80d83da0e158ab5d77872
Hishaq00 <157884178+Hishaq00@users.noreply.github.com>
2024-11-03 22:40:15 -0800

    Add files via upload

--- aside.php
+++ aside.php
@@ -2,7 +2,7 @@
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
     <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
     </div>
@@ -14,7 +14,7 @@
 
 <!-- Nav Item - Dashboard -->
 <li class="nav-item active">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="index.php">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
@@ -32,13 +32,13 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
+        <span>Categories</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.php">Buttons</a>
-            <a class="collapse-item" href="cards.php">Cards</a>
+            <h6 class="collapse-header">Custom Categories:</h6>
+            <a class="collapse-item" href="addcat.php">Add Categories</a>
+            <a class="collapse-item" href="viewcat.php">View Categories</a>
         </div>
     </div>
 </li>
@@ -48,16 +48,15 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
+        <span>Products</span>
     </a>
     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.php">Colors</a>
-            <a class="collapse-item" href="utilities-border.php">Borders</a>
-            <a class="collapse-item" href="utilities-animation.php">Animations</a>
-            <a class="collapse-item" href="utilities-other.php">Other</a>
+            <h6 class="collapse-header">Custom Products:</h6>
+            <a class="collapse-item" href="utilities-color.php">Add Product</a>
+            <a class="collapse-item" href="utilities-border.php">View Product</a>
+      
         </div>
     </div>
 </li>
@@ -332,3 +331,5 @@
 
     </nav>
     <!-- End of Topbar -->
+
+  
--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$con=mysqli_connect("localhost","root","","miniproject");
+$con=mysqli_connect("localhost","root","","project_db");
 if(mysqli_connect_error()){
     echo "<script>alert('connection failed')</script>";
 }else{
-    // echo "<script>alert('connection build successfully')</script>";
+    //  echo "<script>alert('connection build')</script>";
 }
 ?>
